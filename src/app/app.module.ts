669fc08367f7d8427ad2e3fc113535cfb48ae8c7
Renato <rsareas93@gmail.com>
2021-05-27 08:19:26 -0300

    inicial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import {HttpClientModule} from '@angular/common/http';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+const routes : Routes = [
+  { path : 'cadastro-clientes', 
+      component : CadastroClientesComponent },
+  { path : 'consulta-clientes', 
+      component : ConsultaClientesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroClientesComponent,
+    ConsultaClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+   RouterModule.forRoot(routes) 
+   //registrando as rotas criadas no projeto..
   ],
   providers: [],
   bootstrap: [AppComponent]
