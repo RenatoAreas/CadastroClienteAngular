83a1e0849c869f7d9822620d61f82427018d5619
Renato <rsareas93@gmail.com>
2021-05-30 12:21:14 -0300

    Criando listagem de clientes e ações em botoes

--- src/app/consulta-clientes/consulta-clientes.component.ts
+++ src/app/consulta-clientes/consulta-clientes.component.ts
@@ -12,6 +12,16 @@ export class ConsultaClientesComponent implements OnInit {
 
   listagemClientes: any = [];
 
+  cliente = {
+    idCliente : 0,
+    nome: '',
+    email: '',
+    cpf: ''
+  }
+
+  mensagem_edicao:string;
+  mensagem_exclusao:string;
+
   constructor(private httpClient:HttpClient) { }
 
   ngOnInit() : void{
@@ -29,6 +39,51 @@ export class ConsultaClientesComponent implements OnInit {
         console.log(e);
       }
     );
+  }
+
+  obterCliente(id) : void {
+
+    this.mensagem_edicao ="";
+    this.mensagem_exclusao="";
+
+    this.httpClient.get(environment.api_clientes + "/" + id)
+      .subscribe(
+        (data:any) => {
+          this.cliente = data;
+        },
+        e => {
+          console.log(e);
+        }
+      )
+  }
+
+  //atualizando os dados do cliente
+  atualizarCliente(formEdicao) : void{
+    this.httpClient.put(environment.api_clientes,
+      formEdicao.form.value, {responseType :'text'})
+      .subscribe(
+         data => {
+          this.mensagem_edicao = data;
+          this.consultarClientes();
+      },
+      e =>{
+        console.log(e);
+      }
+      );
+  }
+  //excluindo cliente
+  excluirCliente (id) : void {
+    this.httpClient.delete(environment.api_clientes + "/"  + id,
+    {responseType : 'text'})
+    .subscribe(
+      data => {
+        this.mensagem_exclusao = data;
+        this.consultarClientes()
+      },
+      e => {
+        console.log(e);
+      }
+    )
   }
 
 }
